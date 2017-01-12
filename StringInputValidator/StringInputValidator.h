e198a58bff526b59d91a5fcd95e2f33e305c2808
RABE_IT Services <development@berndrabe.de>
2017-01-12 18:18:53 +0100

    Initial Commit

---
+++ StringInputValidator/StringInputValidator.h
@@ -0,0 +1,19 @@
+//
+//  StringInputValidator.h
+//  StringInputValidator
+//
+//  Created by Bernd Rabe on 12.01.17.
+//  Copyright © 2017 RABE_IT Services. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StringInputValidator.
+FOUNDATION_EXPORT double StringInputValidatorVersionNumber;
+
+//! Project version string for StringInputValidator.
+FOUNDATION_EXPORT const unsigned char StringInputValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringInputValidator/PublicHeader.h>
+
+
